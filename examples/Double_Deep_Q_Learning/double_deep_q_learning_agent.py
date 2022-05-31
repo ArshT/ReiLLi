ae75d37da66845961d00993999d1dcd12a8f3e8e
ArshT <40928833+ArshT@users.noreply.github.com>
2022-05-31 20:09:04 +0530

    DDPG Correction + PPO_BiP

--- examples/Double_Deep_Q_Learning/double_deep_q_learning_agent.py
+++ examples/Double_Deep_Q_Learning/double_deep_q_learning_agent.py
@@ -1,26 +1,23 @@
 import numpy as np
 import gym
-from algorithms.DDPG.ddpg import Agent
 import os
 import torch
 from utils.plot import RewardPlot
+from algorithms.Double_Deep_Q_Learning.double_dql import Agent
 
 
-
-class DDPG_agent:
-    def __init__(self,env_name,num_episodes,batch_size,gamma,solved_reward,tau,start_action_std,min_action_std,action_std_decay_rate,
-                 action_std_decay_ep,buffer_size,render,num_test_episodes,actor_alpha,critic_alpha,fc1_dims,fc2_dims,device):
+class Double_DQL_agent:
+    def __init__(self,env_name,num_episodes,batch_size,gamma,solved_reward,replace,epsilon,
+                 buffer_size,render,num_test_episodes,alpha,eps_dec,eps_end,fc1_dims,fc2_dims,device):
 
         self.env = gym.make(env_name)
         input_dims = self.env.observation_space.shape[0]
-        action_dims = self.env.action_space.shape[0]
+        n_actions = self.env.action_space.n
         self.env_name = env_name
 
-        self.agent = Agent(input_dims=input_dims,fc1_dims=fc1_dims,fc2_dims=fc2_dims,action_dims=action_dims,actor_alpha=actor_alpha,critic_alpha=critic_alpha
-                          ,gamma=gamma,start_action_std=start_action_std,min_action_std=min_action_std,action_std_decay_rate=action_std_decay_rate,
-                           tau=tau,batch_size=batch_size,buffer_size=buffer_size,device=device)
-
-        self.action_std_decay_ep = action_std_decay_ep
+        self.agent = Agent(input_dims=input_dims,n_actions=n_actions,alpha=alpha,gamma=gamma,batch_size=batch_size,
+                           replace=replace,epsilon=epsilon,fc1_dims=fc1_dims,fc2_dims=fc2_dims,
+                           buffer_size=buffer_size,eps_dec=eps_dec,eps_end=eps_end,device=device)
 
         self.num_episodes = num_episodes
         self.num_test_episodes = num_test_episodes
@@ -28,14 +25,13 @@ class DDPG_agent:
         self.device = device
         self.render = render
         self.solved_reward = solved_reward
-        self.batch_size = batch_size
-
 
     def train(self,model_dir=None,plot_dir=None):
         scores = []
 
         if plot_dir:
-            plot_graph = RewardPlot(env_name=self.env_name,algo_name="DDPG",save_dir=plot_dir)
+            plot_graph = RewardPlot(env_name=self.env_name,algo_name="DDQL",save_dir=plot_dir)
+
 
         for i in range(self.num_episodes):
             done = False
@@ -45,20 +41,17 @@ class DDPG_agent:
             while not done:
                 action = self.agent.choose_action(observation)
                 observation_,reward,done,_ = self.env.step(action)
-                self.agent.store_transitions(observation,action,reward,observation_,done)
                 score += reward
+                self.agent.store_transitions(observation,action,reward,observation_,done)
                 observation = observation_
-                self.agent.update()
+                self.agent.learn()
 
             scores.append(score)
 
-            if (i+1) % self.action_std_decay_ep == 0:
-                self.agent.decay_std()
-
-            if (i+1)%10 == 0:
+            if (i+1) % 10 == 0:
                 avg_score = np.mean(scores[max(0, i-10):(i+1)])
                 avg_score_100 = np.mean(scores[max(0, i-100):(i+1)])
-                print('episode: ', i+1,'score: ', score,' average_score_10 %.3f' % avg_score,' average_score_100 %.3f' % avg_score_100)
+                print('episode: ', i+1,'score: ', score,' average_score_10 %.3f' % avg_score,' average_score_100 %.3f' % avg_score_100,' epsilon %.3f' % self.agent.epsilon)
                 print()
 
                 if not plot_dir:
@@ -67,19 +60,19 @@ class DDPG_agent:
                         break
 
         if model_dir:
-            save_dir = os.path.join(model_dir,"ddpg_dict_"+self.env_name+".pth")
-            torch.save(self.agent.actor.state_dict(),save_dir)
+            save_dir = os.path.join(model_dir,"ddql_dict_"+self.env_name+".pth")
+            torch.save(self.agent.Q_eval.state_dict(),save_dir)
 
         if plot_dir:
             plot_graph.plot_reward_curve(episode_reward_list=scores)
+
 
     def test(self,model_dir=None):
         total_score = 0
 
         if model_dir:
-            save_dir = os.path.join(model_dir,"ddpg_dict_"+self.env_name+".pth")
-            self.agent.actor.load_state_dict(torch.load(save_dir))
-
+            save_dir = os.path.join(model_dir,"ddql_dict_"+self.env_name+".pth")
+            self.agent.Q_eval.load_state_dict(torch.load(save_dir))
 
         for i in range(self.num_test_episodes):
             done = False
--- examples/ppo_bipedalwalker.py
+++ examples/ppo_bipedalwalker.py
@@ -10,14 +10,14 @@ sys.path.append(directory)
 from algorithms.PPO.ppo_agent import PPO_agent
 
 env_name = "BipedalWalker-v3"
-num_episodes = 5000
+num_episodes = 2000
 gamma = 0.99
 render=True
 num_test_episodes = 50
 fc1_dims = 256
 fc2_dims = 256
 device = 'cpu'
-solved_reward = 285
+solved_reward = 290
 update_batch_size = 10
 actor_alpha = 0.0001
 critic_alpha = 0.0003
@@ -26,8 +26,9 @@ K_epochs = 30
 start_action_std = 0.55
 action_std_decay_rate = 0.05
 min_action_std = 0.05
-action_std_decay_ep = 175
+action_std_decay_ep = 200
 model_dir = r"C:\Users\Arsh Tangri\Desktop\Reinforcement Learning\ReiLLi\models"
+plot_dir = r"C:\Users\Arsh Tangri\Desktop\Reinforcement Learning\ReiLLi\plots"
 
 ppo_agent = PPO_agent(env_name=env_name,continuous=True,num_episodes=num_episodes,update_batch_size=update_batch_size,gamma=gamma,solved_reward=solved_reward,actor_alpha=actor_alpha,critic_alpha=critic_alpha,
                       eps_clip=eps_clip,K_epochs=K_epochs,render=render,num_test_episodes=num_test_episodes,fc1_dims=fc1_dims,fc2_dims=fc2_dims,device=device,
@@ -35,5 +36,5 @@ ppo_agent = PPO_agent(env_name=env_name,continuous=True,num_episodes=num_episode
 
 
 
-ppo_agent.train(model_dir=model_dir)
+ppo_agent.train(model_dir=model_dir,plot_dir=plot_dir)
 ppo_agent.test(model_dir = model_dir)
